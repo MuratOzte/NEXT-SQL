b46deb886baf2174d7bb9a34648e1bcfd1ed5945
MuratOzte <mo2553481@gmail.com>
2024-08-20 19:31:21 +0300

    feat: Add user data insertion to Home component
    This commit adds functionality to insert user data into the database in the Home component. It uses the provided user object to insert the user's id, name, surname, and location into the "public.user" table.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import pool from '@/lib/db';
 
 export default function Home() {
+    const user = {
+        id: 6,
+        name: 'Dali',
+        surname: 'Seli',
+        location: 'Dankara',
+    };
+
     const fetchData = async () => {
         try {
             const client = await pool.connect();
             console.log('connected');
             const result = await client.query('SELECT * FROM public.user');
-            console.log(result.fields);
+            const response = await client.query(
+                'INSERT INTO public.user (id, name, surname, location) VALUES ($1, $2, $3, $4)',
+                [user.id, user.name, user.surname, user.location]
+            );
+            console.log(response);
+            console.log(result.rows);
         } catch (error) {
             console.log(error);
         }
